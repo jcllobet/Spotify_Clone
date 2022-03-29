f0bfb98152fd1bebebb489e8a43c13ba3e90bbd4
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-03-29 16:34:32 +0300

    apple log in finished

--- app.js
+++ app.js
@@ -45,3 +45,17 @@ function getInputs() {
 //     }
 // }
 // onload = getInputs();
+//----------------------------------------------------------------------------------------------------------------------
+//redirect from log in page to home page attempt 4.
+//----------------------------------------------------------------------------------------------------------------------
+// button = document.getElementById("myButton");
+// button.onclick = function () {
+//     const password = document.getElementById("inputPassword3");
+//     const email = document.getElementById("inputEmail3");
+//     const button = document.getElementById("myButton");
+//     if (password.value === "cat123" && email.value === "admin@gmail.com") {
+//         button.onclick(location.href = "/index.html");
+//     } else {
+//         return alert("Wrong email or password try again.");
+//     }
+// }
