d3d7ef23290e7ccfce593a4062f42f3a4277a3e4
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-03-29 16:41:19 +0300

    smol edit

--- app.js
+++ app.js
@@ -7,17 +7,18 @@ window.onload = function () {
     sidenav("sidenav"); // We can add routes, currentRoute if needed
     footer("footer"); // We can add routes, currentRoute if needed
 };
-//redirect from log in page to home page.
-function getInputs() {
-    const password = document.getElementById("inputPassword3");
-    const email = document.getElementById("inputEmail3");
-    const button = document.getElementById("myButton");
-    if (password.value === "cat123" && email.value === "admin@gmail.com") {
-        button.onclick(location.href = "/index.html");
-    } else {
-        return alert("Wrong email or password try again.");
-    }
-}
+//----------------------------------------------------------------------------------------------------------------------
+//redirect from log in page to home page 1.
+// function getInputs() {
+//     const password = document.getElementById("inputPassword3");
+//     const email = document.getElementById("inputEmail3");
+//     const button = document.getElementById("myButton");
+//     if (password.value === "cat123" && email.value === "admin@gmail.com") {
+//         button.onclick(location.href = "/index.html");
+//     } else {
+//         return alert("Wrong email or password try again.");
+//     }
+// }
 //----------------------------------------------------------------------------------------------------------------------
 //redirect from log in page to home page attempt 2.
 //----------------------------------------------------------------------------------------------------------------------
@@ -59,3 +60,17 @@ function getInputs() {
 //         return alert("Wrong email or password try again.");
 //     }
 // }
+//----------------------------------------------------------------------------------------------------------------------
+//redirect from log in page to home page attempt 5.
+//----------------------------------------------------------------------------------------------------------------------
+// const password = document.getElementById("inputPassword3");
+// const email = document.getElementById("inputEmail3");
+// function getInputs(a, b) {
+//     const button = document.getElementById("myButton");
+//     if (a.value === "cat123" && b.value === "admin@gmail.com") {
+//         button.onclick(location.href = "/index.html");
+//     } else {
+//         return alert("Wrong email or password try again.");
+//     }
+// }
+// console.log(getInputs(email, password));
