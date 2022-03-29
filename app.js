e57cc5d9e27c88ccc0302efcde6e348a9a37c911
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-03-29 12:29:39 +0300

    added animation and faulty code
    captcha has slide out animation without added elements, app.js code for switching pages doesn't work

--- app.js
+++ app.js
@@ -7,3 +7,19 @@ window.onload = function () {
     sidenav("sidenav"); // We can add routes, currentRoute if needed
     footer("footer"); // We can add routes, currentRoute if needed
 };
+//redirect from log in page to home page.
+function getInputs(id) {
+    const password = document.getElementById("inputPassword3");
+    const email = document.getElementById("inputEmail3");
+    if (password.value === "cat123" && email.value === "admin@gmail.com") {
+        document.getElementById("myButton").onclick = function () {
+            location.href = "/index.html"
+        }
+    } else {
+        alert("Wrong email or password try again.");
+    }
+}
+//redirect from log in page to home page.
+document.getElementById("myButton").onclick = function () {
+    location.href = "/index.html"
+}
