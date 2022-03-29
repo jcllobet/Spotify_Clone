a8ce960600878dd9204002b1a97610e0b2435ec4
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-03-29 19:24:49 +0300

    update
    sorry if stuff gets broken

--- app.js
+++ app.js
@@ -3,18 +3,36 @@
 import { footer } from "./components/footer.js";
 import { sidenav } from "./components/sidenav.js";
 
-window.onload = function () {
-    sidenav("sidenav"); // We can add routes, currentRoute if needed
-    footer("footer"); // We can add routes, currentRoute if needed
+
+const checkIfValid = (event) => {
+    event.preventDefault();
+    console.log("clicked");
+    let password = document.getElementById("inputPassword3");
+    let email = document.getElementById("inputEmail3");
+    const button = document.getElementById("logInButton");
+    console.log(password.value);
+    console.log(email.value);
+
+    if (password.value === "1" && email.value === "admin@gmail.com") {
+        location.href = "/index.html"
+        return true;
+    } else {
+        return alert("Wrong email or password try again.");
+    }
 };
+
+
+// Add a listener to the button
+const addLoginListener = () => {
+    const button = document.getElementById("logInButton");
+    button.addEventListener("click", checkIfValid);
+    console.log("added listener");
+}
+
 //----------------------------------------------------------------------------------------------------------------------
-// document.getElementById("logInButton").onclick = function () {
-//     let password = document.getElementById("inputPassword3");
-//     let email = document.getElementById("inputEmail3");
-//     const button = document.getElementById("logInButton");
-//     if (password.value === "cat123" && email.value === "admin@gmail.com") {
-//         button.onclick(location.href = "/index.html");
-//     } else {
-//         return alert("Wrong email or password try again.");
-//     }
-// }
+window.onload = function () {
+    // sidenav("sidenav"); // We can add routes, currentRoute if needed
+    // footer("footer"); // We can add routes, currentRoute if needed
+    // add listener to the login button
+    addLoginListener();
+};
