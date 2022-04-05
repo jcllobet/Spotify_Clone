094341de271e07e49c547a0de9e75c9f6660ecb5
Jan Carbonell <jcllobet@gmail.com>
2022-04-05 16:42:28 +0200

    managed to do the api call

--- functions/getArtistData.js
+++ functions/getArtistData.js
@@ -1,23 +1,23 @@
 const fetch = (...args) =>
     import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-const getArtistData = (artistName) => {
-    const options = {
-        method: "GET",
-        headers: {
-            "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-            "X-RapidAPI-Key": process.env.RAPID_KEY,
-        },
-    };
-
-    fetch(
-        `https://deezerdevs-deezer.p.rapidapi.com/search?q=${artistName}`,
-        options
-    )
-        .then((response) => response.json())
-        .then((response) => console.log(response))
-        .catch((err) => console.error(err));
+const options = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+        "X-RapidAPI-Key": process.env.RAPID_KEY,
+    },
 };
+
+async function getArtistData(artistName) {
+    const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/deezer/search?q=${artistName}`
+    );
+    const formattedResponse = await response.json();
+    //console.log(formattedResponse);
+    const dataArr = await formattedResponse["data"];
+    console.log(dataArr[0]);
+}
 
 getArtistData("eminem");
 
